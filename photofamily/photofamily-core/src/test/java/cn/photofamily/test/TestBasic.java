664127f7a6ea006113159c441f137008a591786d
wangyonghe <602383270@qq.com>
2016-06-25 10:16:23 +0800

    发布项目

---
+++ photofamily/photofamily-core/src/test/java/cn/photofamily/test/TestBasic.java
@@ -0,0 +1,25 @@
+/**
+ * Project Name:photofamily-core
+ * File Name:TestBasic.java
+ * Package Name:cn.photofamily.test
+ * Date:2016年6月25日上午1:29:14
+ * Copyright (c) 2016, ehking All Rights Reserved.
+ *
+*/
+
+package cn.photofamily.test;
+/**
+ * ClassName:TestBasic <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason:	 TODO ADD REASON. <br/>
+ * Date:     2016年6月25日 上午1:29:14 <br/>
+ * @author   wangyonghe
+ * @version  
+ * @since    JDK 1.7
+ * @see 	 
+ */
+public class TestBasic {
+    public static void main(String[] args) {
+    }
+}
+
---
+++ photofamily/photofamily-web/src/main/java/cn/pothofamily/commons/controller/BasicController.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:photofamily-web File Name:BasicController.java Package
+ * Name:cn.pothofamily.commons.controller Date:2016年6月25日上午3:07:14 Copyright (c) 2016, ehking All
+ * Rights Reserved.
+ *
+ */
+
+package cn.pothofamily.commons.controller;
+
+/**
+ * ClassName:BasicController <br/>
+ * Function: TODO ADD FUNCTION. <br/>
+ * Reason: TODO ADD REASON. <br/>
+ * Date: 2016年6月25日 上午3:07:14 <br/>
+ * 
+ * @author wangyonghe
+ * @version
+ * @since JDK 1.7
+ * @see
+ */
+public class BasicController {
+    public static void main(String[] args) {
+        
+    }
+}
+
